6f6d2dfbb5f7778962b4f085d288c918a7604de8
Thomas Macquart <thomas.macquart@Thomass-MacBook-Pro.local>
2018-06-07 13:22:22 +0200

    log refresh token

--- app/src/main/java/beyondbanking/com/bestbank/activities/MyFirebaseInstanceIDService.kt
+++ app/src/main/java/beyondbanking/com/bestbank/activities/MyFirebaseInstanceIDService.kt
@@ -1,6 +1,17 @@
 package beyondbanking.com.bestbank.activities
 
+import android.util.Log
 import com.google.firebase.iid.FirebaseInstanceIdService
+import com.google.firebase.iid.FirebaseInstanceId
+
+
 
 class MyFirebaseInstanceIDService : FirebaseInstanceIdService() {
+
+    override fun onTokenRefresh() {
+        super.onTokenRefresh()
+
+        val refreshedToken = FirebaseInstanceId.getInstance().token
+        Log.d("thomas", "Refreshed token: " + refreshedToken!!)
+    }
 }
